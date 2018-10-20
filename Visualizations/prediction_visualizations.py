a2aa44c300d24dc7cb5cf7f4003faba86aa64a26
Philip Stoeber <Philip@Philips-MBP-2.home>
2018-10-20 19:37:58 -0400

    adding visualization script and sql

---
+++ Visualizations/prediction_visualizations.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import pymysql
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+
+def extract_sql(file_path):
+    with open(file_path, 'r') as infile:
+        return [i for i in infile.readlines()]
+
+def create_cmd_str(query):
+    return ' '.join([i for i in query])
+
+def extract_data(conn, sql):
+    return pd.read_sql(sql=sql, con=conn, index_col=None)
+
+def player_pred_to_actual_pts(df):
+    sns.set_style('whitegrid')
+    plt.title('Predicted Points Vs. Actual Points')
+    df.groupby(['team', 'game_date'])
+    sns.barplot(x='name', y='pts', data=df, hue='flag', palette='plasma_r')
+    plt.show()
+
+def win_lose_prob(df):
+    plt.title('Win Probability Vs. Lose Probability')
+    labels = 'lose_prob', 'win_prob'
+    colors = ['red', 'green']
+    explode = (.2, 0)
+    plt.pie(df.iloc[0], explode=explode, labels=labels, colors=colors, autopct='%1.11f%%', shadow=True, startangle=140)
+    plt.show()
+
+def plot_total_pts_pred(df):
+    plt.title('Predicted Total Points Vs Actual Total Points')
+    sns.lineplot(x='game_date', y='predicted_total_pts', data=df, color='red', legend='brief')
+    sns.lineplot(x='game_date', y='r_squared', data=df, color='blue')
+    plt.show()
+
+#def
+
+if __name__ == '__main__':
+    try:
+        connection = pymysql.connect(host='localhost', user='root', password='Sk1ttles', db='nba_stats_test')
+    except:
+        print('Failed to connect to database')
+        sys.exit(1)
+
+    #player_pred_df = extract_data(connection, create_cmd_str(extract_sql(sys.argv[1])))
+    #player_pred_to_actual_pts(player_pred_df)
+    #win_lose_prob_df = extract_data(connection, create_cmd_str(extract_sql(sys.argv[2])))
+    #win_lose_prob(win_lose_prob_df)
+    total_pts_pred_df = extract_data(connection, create_cmd_str(extract_sql(sys.argv[3])))
+    plot_total_pts_pred(total_pts_pred_df)
