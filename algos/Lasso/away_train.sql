2143b584be65346ed79e5bb454aae3977eb85603
Philip Stoeber <Philip@Philips-MacBook-Pro-2.local>
2018-11-17 22:49:23 -0500

    updates to lasso queries

--- algos/Lasso/away_train.sql
+++ algos/Lasso/away_train.sql
@@ -1,3 +1,4 @@
+
 select basic.player_id,
        box_view.team,
        box_view.game_hash,
@@ -26,31 +27,18 @@ select basic.player_id,
        reg_avg.blk as tot_blk,
        reg_avg.stl as tot_stl,
        reg_avg.pf as tot_pf,
-       reg_avg.`TO` as tot_to,
-       opp_team_pts.opp_pts,
-       opp_team_pts.diff
+       reg_avg.`TO` as tot_to
 from (
 
      select m.game_hash, m.team, m.game_date, m.home_away, lu.season
      from box_scores_map_view as m
-     inner join game_date_lookup as lu on m.game_date = lu.day
+            inner join game_date_lookup as lu on m.game_date = lu.day
      where m.game_date < current_date and
-           lu.season > 2010 and
+           lu.season > 2009 and
            m.home_away = 'Away'
      order by game_date desc
 
      ) as box_view
-
-inner join(
-
-    select b.game_hash, team.team_id, lu.season
-    from box_score_map as b
-    inner join game_date_lookup as lu on b.game_date = lu.day
-    inner join team_info as team on b.home_team = team.team
-    where b.game_date < current_date and
-          lu.season > 2010
-
-    ) as home_team on box_view.game_hash = away_team.game_hash
 
 inner join player_team_map as play_m on ( (box_view.team = play_m.team) and (
 box_view.season = play_m.season) )
@@ -60,5 +48,4 @@ inner join advanced_box_stats as adv on ( (box_view.game_hash = adv.game_hash) a
 play_m.player_id = adv.player_id) )
 inner join team_advanced_boxscore_stats as a_stats on ( (box_view.game_hash = a_stats.game_hash) and (box_view.team = a_stats.team) )
 left outer join RegularSeasonAverages as reg_avg on ( (basic.player_id = reg_avg.player_id) and (box_view.season -1 = reg_avg.season) )
-inner join points as opp_team_pts on home_team.team_id = opp_team_pts.team_id and away_team.season -1 = opp_team_pts.season
 order by box_view.game_date desc;
